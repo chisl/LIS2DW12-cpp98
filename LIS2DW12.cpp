47cf63fd3fd71ce074961494f2872ba3c1e0bbc2
chisl <info@chisl.io>
2018-02-05 19:15:39 +0100

    Initial commit

---
+++ LIS2DW12.cpp
@@ -0,0 +1,18 @@
+/*
+ * name:        LIS2DW12
+ * description: MEMS digital output motion sensor: high-performance ultra-low-power 3-axis 'femto' accelerometer
+ * manuf:       ST Microelectronics
+ * version:     Version 0.1
+ * url:         http://www.st.com/resource/en/datasheet/lis2dw12.pdf
+ * date:        2018-02-05
+ * author       https://chisl.io/
+ * file:        LIS2DW12.cpp
+ */
+
+/*                                                                                                       *
+ *                                   THIS FILE IS AUTOMATICALLY CREATED                                  *
+ *                                    D O     N O T     M O D I F Y  !                                   *
+ *                                                                                                       */
+
+#include "LIS2DW12.hpp"
+
